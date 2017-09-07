ff6805cd5348a7252a64283f4ad5a45107899117
joaquin <joaquinollo@gmail.com>
2017-09-06 21:57:00 -0300

    commit inicial de proyecto avanzado

---
+++ game.py
@@ -0,0 +1,64 @@
+from player import Player
+from collections import OrderedDict
+import world
+
+# continue at chapter 13, solve bug right before starting with "expanding the world"
+
+def play():
+    player = Player()
+    print ("Escape from Cave terror!")
+    while True:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
+        room.modify_player(player)
+        choose_action(room,player)
+
+def get_available_actions(room, player):
+    actions = OrderedDict()
+    print("Choose an action: ")
+    if player.inventory:
+        action_adder(actions, 'i', player.print_inventory, "Print inventory")
+    if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
+        action_adder(actions, 'a', player.attack, "Attack")
+    else:
+        if world.tile_at(room.x, room.y -1):
+            action_adder(actions, 'n', player.move_north, "Go north")
+        if world.tile_at(room.x, room.y +1):
+            action_adder(actions, 's', player.move_south, "Go south")
+        if world.tile_at(room.x+1, room.y):
+            action_adder(actions, 'e', player.move_east, "Go east")
+        if world.tile_at(room.x-1, room.y):
+            action_adder(actions, 'w', player.move_west, "Go west")
+    if player.hp < 100:
+        action_adder(actions, 'h', player.heal, "Heal")
+    return actions
+
+def action_adder(action_dict, hotkey, action, name):
+    action_dict[hotkey.lower()] = action
+    action_dict[hotkey.upper()] = action
+    print("{}: {}".format(hotkey,name))
+
+def choose_action(room,player):
+    action = None
+    while not action:
+        available_actions = get_available_actions (room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
+        else:
+            print ("Invalid action!")
+
+def most_powerful_weapon(inventory):
+    max_damage=0
+    best_weapon=None
+    for item in inventory:
+        try:
+            if item.damage > max_damage:
+                best_weapon = item
+                max_damage = item.damage
+        except AttributeError:
+            pass
+        return best_weapon
+
+play()
